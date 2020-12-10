b2256b7aa8de53f4ab6ddf7389d46af0f0829f49
Herman <nitrowitty@gmail.com>
2020-12-10 16:18:08 -0600

    structs

--- module1/budget_1.go
+++ module1/budget_1.go
@@ -2,4 +2,13 @@ package module1
 
 // Budget stores budget information
 
+type Budget struct {
+	Max int32
+	items []Item
+}
+
 // Item stores item information
+type Item struct {
+	Description string
+	Price float32
+}
